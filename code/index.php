09d1584c58d20089f0f01f0e2835a3860c140deb
BayborodinAlexandr <albayborodin@yandex.ru>
2024-10-03 22:14:55 +0300

    Домашняя работа по первому семинару по  PHP

---
+++ code/index.php
@@ -0,0 +1,14 @@
+<?php
+// $a = 5;
+// $b = '05';
+// var_dump($a == $b);
+// var_dump((int)'012345');
+// var_dump((float)123.0 === (int)123.0);
+// var_dump(0 == 'hello, world');
+$a = 1;
+$b = 2;
+$a = $a + $b; 
+$b = $a - $b; 
+$a = $a - $b; 
+
+echo "a = $a, b = $b"; // Вывод: a = 2, b = 1
